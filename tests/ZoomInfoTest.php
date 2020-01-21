8cd2d055cebdd8312e81c93b58946ee8eaabfc60
Robert Lester <rlester@liveperson.com>
2020-01-21 11:31:28 -0500

    ..

--- tests/ZoomInfoTest.php
+++ tests/ZoomInfoTest.php
@@ -5,14 +5,35 @@ use ZoomInfo\ZoomInfo;
 
 class ZoomInfoTest extends TestCase
 {
-    public function testAuthentication()
+    public function testGetCompany()
     {
         $zi = new ZoomInfo([
-
+            'username' => getenv('ZI_USER'),
+            'password' => getenv('ZI_PASS')
         ]);
 
-        $this->assertTrue($zi != null);
+        $company = $zi->enrich->company->get([
+            'matchCompanyInput' => [
+                [
+                    'companyWebsite' => 'github.com'
+                ]
+            ],
+            'outputFields' => [
+                'name',
+                'revenue',
+                'employeeCount',
+                'logo',
+                'website',
+                'createdDate',
+                'companyStatus',
+                'companyStatusDate',
+                'phone',
+                'descriptionList',
+                'country',
+                'ticker'
+            ]
+        ]);
 
-        
+        $this->assertTrue($company->success);
     }
 }
